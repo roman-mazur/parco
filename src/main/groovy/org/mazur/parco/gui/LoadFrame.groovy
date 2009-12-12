9789831912931a33b6edfa88f564af941a76b52a
mazur.roman <mazur.roman@9fa775a2-a520-11de-bb10-370f7287a6b7>
2009-12-12 21:53:56 +0000

    loader

--- src/main/groovy/org/mazur/parco/gui/LoadFrame.groovy
+++ src/main/groovy/org/mazur/parco/gui/LoadFrame.groovy
@@ -2,8 +2,11 @@ package org.mazur.parco.gui;
 
 import groovy.swing.SwingBuilder;
 import java.awt.BorderLayout;
+
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import org.mazur.parco.loader.LoadStep;
+import org.antlr.runtime.tree.CommonTree;
 import org.mazur.parco.visualizer.Vizualizer;
 import java.awt.Image;
 
@@ -11,29 +14,40 @@ public class LoadFrame {
   /** Swing builder. */
   private SwingBuilder swing = new SwingBuilder()
   
-  private JLabel imageLabel
+  private JLabel imageLabel, timeLabel, stepLabel
   
-  private List<Image> loadSteps
+  private List<Image> loadStepImages
+  private List<LoadStep> loadSteps
   
-  private def stepAction = swing.action() {
+  private int stepCounter = 0
+  
+  private def stepAction = swing.action(
     name : "Step",
     closure : {
       if (!loadSteps) { return }
-      Image stepImage = loadSteps.remove(0)
-      
+      stepCounter++
+      Image stepImage = loadStepImages.remove(0)
+      LoadStep step = loadSteps.remove(0)
+      println "Next step"
+      imageLabel.icon = new ImageIcon(stepImage)
+      timeLabel.text = "$step.duration"
+      stepLabel.text = "$stepCounter"
     }
-  }
+  )
   
   void show(final List<LoadStep> loadSteps, int n, final CommonTree root) {
-    this.loadSteps = new ArrayList<Image>(loadSteps.size())
+    this.loadStepImages = new ArrayList<Image>(loadSteps.size())
+    this.loadSteps = loadSteps
     for (LoadStep step in loadSteps) {
-      this.loadSteps += Vizualizer.getImage(root)
+      this.loadStepImages += Vizualizer.getImage(root, step)
     }
     (swing.frame(title : "Loader", pack : true) {
       borderLayout()
       panel(constraints : BorderLayout.NORTH) {
         vbox {
           label("Count of processors: $n")
+          hbox() { label("Time: "); timeLabel = label() }
+          hbox() { label("Step: "); stepLabel = label() }
           button(action : stepAction)
         }
       }
